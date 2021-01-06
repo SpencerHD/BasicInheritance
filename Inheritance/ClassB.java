68ed330a9e947bb943c5ce34508d5df5cf14d0e8
Spencer Huebler-Davis <39494772+SpencerHD@users.noreply.github.com>
2021-01-06 14:31:31 -0500

    Add files via upload

---
+++ Inheritance/ClassB.java
@@ -0,0 +1,10 @@
+// Create a subclass by extending class A.
+class B extends A {
+	int k;
+	void showk() {
+		System.out.println("k: " + k);
+	}
+void sum() {
+	System.out.println("i+j+k: " + (i+j+k));
+	}
+}
---
+++ Inheritance/ClassC.java
@@ -0,0 +1,10 @@
+
+class C extends B {
+	int m;
+	void showm() {
+		System.out.println("m: " + m);
+	}
+	void sum() {
+		System.out.println("i+j+k+m: " + (i+j+k+m));
+	}
+}
